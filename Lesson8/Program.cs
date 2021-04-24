0472959a3ca377d2a04e8a5a220f3734e784eb6b
votarik <alexspb41@gmail.com>
2021-04-24 12:58:53 +0300

    add Lesson8

---
+++ Lesson8/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Greeting;
+
+namespace Lesson8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Как тебя зовут?");
+            string name = Console.ReadLine();
+            Hello.GreedOfficial(name);
+            Hello.GreedNonofficial(name);
+        }
+    }
+}
